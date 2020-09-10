9a0a693673a1c762611408288d66a41dc34e67a0
Akira <smxshxishxad@me.com>
2020-09-11 07:05:16 +0900

    :recycle: Call fizzbuzz()

--- fizzbuzz/fizzbuzz_worldcup.py
+++ fizzbuzz/fizzbuzz_worldcup.py
@@ -1,14 +1,9 @@
+from fizzbuzz import fizzbuzz
+
 players = ['A', 'B', 'C', 'D', 'E', 'F']
 n_round = 3
 
 for i, player in enumerate(players * n_round):
     turn = i + 1
-    msg = player + ': '
-    if turn % 3 == 0 or turn % 5 == 0:
-        if turn % 3 == 0:
-            msg = msg + 'Fizz'
-        if turn % 5 == 0:
-            msg = msg + 'Buzz'
-    else:
-        msg = msg + str(turn)
+    msg = player + ': ' + fizzbuzz(turn)
     print(msg)
