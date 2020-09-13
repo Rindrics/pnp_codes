b0302566cc6380bd62f7442f16efb846f733b7e0
Akira <smxshxishxad@me.com>
2020-09-13 17:03:50 +0900

    :recycle: Call generate_fizzbuzz_msg()

--- fizzbuzz/fizzbuzz_worldcup.py
+++ fizzbuzz/fizzbuzz_worldcup.py
@@ -1,13 +1,9 @@
+from fizzbuzz import generate_fizzbuzz_msg
+
 players = ['A', 'B', 'C', 'D']
 n_round = 3
 
 for i, player in enumerate(players * n_round):
     turn = i + 1
-    msg  = ''
-    if turn % 3 == 0:
-        msg += 'Fizz'
-    if turn % 5 == 0:
-        msg += 'Buzz'
-    if msg == '':
-        msg += str(turn)
+    msg  = generate_fizzbuzz_msg(turn)
     print(player + ': ' + msg)
